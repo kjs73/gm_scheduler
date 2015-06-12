0009e836dca5c014771bb218a7c8d25fa54e49af
Julian Schrenk <kjs73@cam.ac.uk>
2015-06-12 18:49:02 +0100

    add group meeting parameters

--- gm_scheduler.cpp
+++ gm_scheduler.cpp
@@ -3,6 +3,8 @@
 #include "gm_scheduler.hpp"
 
 gm_scheduler::gm_scheduler()
+    : nr_weeks_to_schedule("number of weeks to schedule"),
+      nr_speakers_per_week("number of speakers per week")
 {
     std::cout << "GROUP MEETING SCHEDULER\n";
 }
--- gm_scheduler.hpp
+++ gm_scheduler.hpp
@@ -1,11 +1,12 @@
-#include <string>
+#include "parameter.hpp"
 
 class gm_scheduler {
 public:
     typedef unsigned int week_t;
+    typedef unsigned int speaker_nr_t;
     typedef std::string name_t;
-private:
-    week_t nr_weeks_to_schedule;
-public:
     gm_scheduler();
+private:
+    parameter<week_t> nr_weeks_to_schedule;
+    parameter<speaker_nr_t> nr_speakers_per_week;
 };
